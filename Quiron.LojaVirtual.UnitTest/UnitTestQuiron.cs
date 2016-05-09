14c89878cdbd31a0adab7e3a81467d1a9dbbea25
GabrielNF <gabriel.naldi.f@hotmail.com>
2016-05-09 17:58:39 -0300

    Aula 12

--- Quiron.LojaVirtual.UnitTest/UnitTestQuiron.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTestQuiron.cs
@@ -1,18 +1,41 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Web.Models;
+using Quiron.LojaVirtual.Web.HtmlHelpers;
 
 namespace Quiron.LojaVirtual.UnitTest
 {
-    [TestClass]
-    public class UnitTestQuiron
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
+    
+        //[TestMethod]
 
+        //public void TestarSeAPaginacaoEstaSendoGeradaCorretamente()
+        //{
 
+        //    HtmlHelper html = null;
 
+        //    Paginacao paginacao = new Paginacao
+        //    {
+        //        PaginaAtual = 2,
+        //        ItensPorPagina = 10,
+        //        ItensTotal = 28
+        //    };
 
-        }
-    }
+            
+
+        //    Func<int, string> paginaUrl = i => "Pagina" + i;
+
+        //    //Act
+
+        //    MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
+
+        ////Assert
+
+        //Assert.AreEqual(
+        //    @"<a class=""btn btn-default"" href=""Paginal"">1</a>"
+        //        + @"<a class=""btn btn-default btn-primary selected""href =""Pagina2"">2</a>"
+        //        + @"<a class=""btn btn-default"" href=""Pagina3"">3</a>", resultado.ToString()
+        //        );
+        //}
+    
 }
--- Quiron.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
+++ Quiron.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -10,14 +10,13 @@ namespace Quiron.LojaVirtual.Web.HtmlHelpers
 {
     public static class PaginacaoHelpers
     {
-        private static object resultado;
 
         public static MvcHtmlString PageLinks(this HtmlHelper html, Paginacao paginacao, Func<int, string> paginaUrl)
         {
             StringBuilder resultado = new StringBuilder();
 
 
-            for (int i = 1; i < paginacao.TotalPagina; i++)
+            for (int i = 1; i <= paginacao.TotalPagina; i++)
             {
                 TagBuilder tag = new TagBuilder("a");
                 tag.MergeAttribute("href", paginaUrl(i));
@@ -26,7 +25,7 @@ namespace Quiron.LojaVirtual.Web.HtmlHelpers
                 if (i == paginacao.PaginaAtual)
                 {
                     tag.AddCssClass("selected");
-                    tag.AddCssClass("btn.primary");
+                    tag.AddCssClass("btn-primary");
                 }
                 tag.AddCssClass("btn btn-default");
                 resultado.Append(tag);
